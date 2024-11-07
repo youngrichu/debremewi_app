f0858ddfb5a1a2bec1ec0ff2d0a2111ad1ec4785
youngrichu <myabiti@gmail.com>
2024-11-08 01:29:17 +0400

    refactor: Replace AuthScreen with AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
-import AuthScreen from './src/screens/AuthScreen';
-import { store } from './src/store'; // Adjust the path if your store is located elsewhere
+import AppNavigator from './src/navigation/AppNavigator';
+import { store } from './src/store';
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <AuthScreen />
+        <AppNavigator />
       </NavigationContainer>
     </Provider>
   );
